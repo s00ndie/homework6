1e45c9ca4d423340fb17d01331919460b17dc1b6
s00ndie <s00ndie@icloud.com>
2024-09-03 16:02:18 +0200

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,12 @@
+data = int(input('Enter number 0-8640000:'))
+f_days = 24*60*60
+f_hours = 60*60
+f_minutes = 60
+days= data // f_days
+hours= data % f_days // f_hours
+minutes= data % f_days % f_hours // f_minutes
+seconds= data % f_days % f_hours % f_minutes
+if days == 1:
+    print(f'{days} day {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}')
+else:
+    print(f'{days} days {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}')
